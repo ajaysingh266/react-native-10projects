048da287af4586367b1dc379b98531d52bf7fe18
ajaysingh266 <ajaysingh266@gmail.com>
2023-04-24 21:54:52 +0530

    first commit of rollingDice

---
+++ screen/05-FeedbackandImages/RollTheDice.tsx
@@ -0,0 +1,58 @@
+import {StyleSheet, Text, View, ImageSourcePropType, Image} from 'react-native';
+import type {PropsWithChildren} from 'react';
+import React from 'react';
+import DiceOne from '../../assets/One.png';
+import DiceTwo from '../../assets/Two.png';
+import DiceThree from '../../assets/Three.png';
+import DiceFour from '../../assets/Four.png';
+import DiceFive from '../../assets/Five.png';
+import DiceSix from '../../assets/Six.png';
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType;
+}>;
+
+const Dice = ({imageUrl}: DiceProps): JSX.Element => {
+  return (
+    <View>
+      <Image style={styles.diceImage} source={imageUrl} />
+    </View>
+  );
+};
+
+const RollTheDice = (): JSX.Element => {
+  return (
+    <View>
+      <Text>RollTheDice</Text>
+    </View>
+  );
+};
+
+export default RollTheDice;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff2f2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#e5e0ff',
+    fontSize: 16,
+    color: '#8ea7e9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
